18cb4146f34111906da9f8e7e226b6652d1ac685
Matt Harris <matrharr@gmail.com>
2016-04-09 14:56:11 -0700

    add basic routes

---
+++ app/controllers/founds_controller.rb
@@ -0,0 +1,33 @@
+class FoundsController < ApplicationController
+
+  def index
+    @found_items = Found.all
+  end
+
+  def show
+    @found_item = Found.find(params[:id])
+  end
+
+  def found_search
+    #POST
+    @found_items = Found.where()
+  end
+
+  def search_results
+    #Display Results
+  end
+
+  def new
+    #Form for new found item
+  end
+
+  def create
+    #POST to create new found item
+    @found_item = Found.new(found_params)
+  end
+
+  private
+  def found_params
+    params.require(:found_item).permit(:user_id, :title,:brand, :category, :type, :color, :description, :location, :photo)
+  end
+end
--- app/controllers/lost_controller.rb
+++ app/controllers/lost_controller.rb
@@ -1,8 +1,28 @@
 class LostsController < ApplicationController
+
   def index
+    @lost_items = lost.all
   end
 
   def show
+    @lost_item = lost.find()
   end
 
+  def lost_search
+  end
+
+  def search_results
+  end
+
+  def new
+  end
+
+  def create
+    @lost_item = lost.new(lost_params)
+  end
+
+  private
+  def lost_params
+    params.require(:lost_item).permit(:user_id, :title,:brand, :category, :type, :color, :description, :location)
+  end
 end
